f05548c1b3f3b190721104d035352946c22a76b7
mrnazu <samuelamsalu1998@gmail.com>
2023-10-30 18:35:34 +0300

    advanced #BOT ONLINE

--- app.js
+++ app.js
@@ -4,9 +4,19 @@ require('dotenv').config();
 
 // creating a new client
 const client = new discord.Client({
-    intents: ['Guilds', 'DirectMessages', 'GuildInvites', 'GuildMessageReactions'], 
+    intents: ['Guilds', 'DirectMessages', 'GuildInvites', 'GuildMessageReactions'],
     partials: ['CHANNEL', 'GUILD_MEMBER', 'MESSAGE', 'REACTION', 'USER']
 });
 
-
-client.login(process.env.TOKEN);
+// bot is online now
+try {
+    const connect = async function () {
+        await client.login(process.env.TOKEN)
+            .then(client.on('ready', function (client) {
+                console.log(`The "${client.user.username}" is Online!`);
+            }));
+    }
+    connect();
+} catch (error) {
+    console.log(`The "${client.user.username}" is NOT Online!'n${error}`);
+}
